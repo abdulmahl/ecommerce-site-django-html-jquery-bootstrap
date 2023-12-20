db8a290b0f5ba5146ac031ce5951380df2781ba6
Abdul Mahl <mdu_abdul@icloud.com>
2023-12-20 11:00:47 +0200

    .

--- cart/views.py
+++ cart/views.py
@@ -29,8 +29,7 @@ def cart_add(request):
 
         # Return response
         # response = JsonResponse({"Product Name: " : product.name})
-        response = JsonResponse({"qty" : cart_quantity})
-
+        response = JsonResponse({"qty":cart_quantity})
         return response
 
 
@@ -38,4 +37,14 @@ def cart_delete(request):
     pass
 
 def cart_update(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get("action") == "post":
+        # Get stuff
+        product_id = int(request.POST.get("product_id"))
+        product_qty = int(request.POST.get("product_qty"))
+
+        cart.update(product=product_id, quantity=product_qty)
+
+        response = JsonResponse({'qty':product_qty})
+        return response
+        # return redirect("cart_summary")
